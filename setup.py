dd6fae080a4fce871db9a1b94f7f1a9af36308c5
Marcus Crane <marcus@utf9k.net>
2021-08-02 14:09:36 +1200

    Test split between Test PyPI and real PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 import os
 from setuptools import find_packages, setup
 
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
 with open(
-    f"{os.path.dirname(os.path.realpath(__file__))}/netbox_plugin_azuread/VERSION", "r"
+    f"{current_dir}/netbox_plugin_azuread/VERSION", "r"
 ) as file:
     VERSION = file.read().strip()
+
+with open(f"{current_dir}/README.md", "r") as file:
+    LONG_DESCRIPTION = file.read()
+
 
 setup(
     name='netbox-plugin-azuread',
     version=VERSION,
     description='Authenticate with Netbox via AzureAD',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     url='https://github.com/marcus-crane/netbox-plugin-azuread',
     author='Marcus Crane',
     license='MIT',
